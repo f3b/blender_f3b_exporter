9c0111bcd3ba24bb6135006930dafd0eb389b2c3
Riccardo Balbo <riccardo@forkforge.net>
2017-02-28 15:44:23 +0100

    Remove xbuf header leftovers

--- src/__init__.py
+++ src/__init__.py
@@ -1,20 +1,5 @@
-# This file is part of blender_io_f3b.  blender_io_f3b is free software: you can
-# redistribute it and/or modify it under the terms of the GNU General Public
-# License as published by the Free Software Foundation, version 2.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-# details.
-#
-# You should have received a copy of the GNU General Public License along with
-# this program; if not, write to the Free Software Foundation, Inc., 51
-# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-#
-# Copyright David Bernard
-
-# <pep8 compliant>
-
+# Based on XBUF exporter 
+# TODO add full license and credits
 
 bl_info = {
     "name": "F3b Exporter",
